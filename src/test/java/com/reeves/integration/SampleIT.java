ca92b6ec76095a400b9efca840a3756e52ceab93
Huang Liang <exceedhl@gmail.com>
2011-04-01 21:11:22 +0800

    added integration test with jetty

---
+++ src/test/java/com/reeves/integration/SampleIT.java
@@ -0,0 +1,10 @@
+package com.reeves.crud.domain;
+
+import junit.framework.TestCase;
+
+public class SampleIT extends TestCase {
+
+    public void test_first_name() throws Exception {
+		assertEquals("a", "1");
+    }
+}
